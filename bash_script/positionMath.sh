ff19a5079ead585d2b4a928c571bb122f74456c0
MohamedAboElYazeed <mohamedaboelyazeed920@gmail.com>
2024-10-01 03:21:45 +0300

    add file bash scripting 1/10

---
+++ bash_script/positionMath.sh
@@ -0,0 +1,10 @@
+#!bin/bash 
+
+x=$1
+y=$2
+
+echo `expr $x + $y`
+echo `expr $x - $y`
+echo `expr $x \* $y`
+echo `expr $x / $y`
+
---
+++ bash_script/readfullname.sh
@@ -0,0 +1,5 @@
+echo "please enter first name"
+read fname
+echo "please enter second name"
+read sname
+echo "Welcom "$fname $sname
